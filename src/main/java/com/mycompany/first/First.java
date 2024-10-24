8cc49cc0469fad45f8bef845004b85bc3158d4e9
sheikh-shishir <ahmed2305101594@diu.edu.bd>
2024-10-24 23:42:39 +0600

    first commit done

---
+++ src/main/java/com/mycompany/first/First.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author macbookair
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello This is My First Project");
+    }
+}
